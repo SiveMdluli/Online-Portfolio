76147e3f8bf061575adb2a7ccfd04ad86196fb4b
Sive Mdluli <mdlulisive202@gmail.com>
2023-10-12 07:50:07 +0200

    Fix JS errro: MIME type ('text/html')

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { initNavbar, navbarlinks } from './modules/navbar.js';
+import { initNavbar } from './modules/navbar.js';
 import { initMobileNav } from './modules/mobileNav.js';
 import { initScrollOnLinks } from './modules/scrollOnLinks.js';
 import { initScrollOnPageLoad } from './modules/scrollOnPageLoad.js';
@@ -7,9 +7,7 @@ import { initTypedEffect } from './modules/typedEffect.js';
 import { initSkillsAnimation } from './modules/skillsAnimation.js';
 import { initPortfolio } from './modules/portfolio.js';
 import { initLightbox } from './modules/lightbox.js';
-
 import { initAnimationOnScroll } from './modules/animationOnScroll.js';
-import { initPureCounter } from './modules/pureCounter.js';
 
 // To ensure that it runs after the HTML document has fully loaded
 document.addEventListener('DOMContentLoaded', () => {
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
 
   output: {
     filename: '[name].js',
+    publicPath: '/',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
