46934861f980c020380d7721a37ee3b75e9ad5e8
Sive Mdluli <mdlulisive202@gmail.com>
2023-10-16 16:12:34 +0200

    Add popup modal feature for each corresponding project image Dynamically

--- modules/portfolio.js
+++ modules/portfolio.js
@@ -32,6 +32,45 @@ export const initPortfolio = () => {
         },
         true
       );
+
+      // Selecting items from modal template
+      const modal = select('#projectModal');
+      const modalImage = modal.querySelector('img');
+      const modalDescription = modal.querySelector('p');
+      const modalBuildWith = modal.querySelector('ul');
+      const modalSourceButton = modal.querySelector('#source-button');
+      const modalLiveButton = modal.querySelector('#live-button');
+      // Click event listener for portfolio items
+      const portfolioItems = document.querySelectorAll('.portfolio-item');
+      portfolioItems.forEach((item) => {
+        item.addEventListener('click', () => {
+          const image = item.querySelector('img');
+          const description = image.getAttribute('data-description');
+          const buildWith = image.getAttribute('data-build-with');
+          const githubLink = image.getAttribute('data-github-link');
+          const liveLink = image.getAttribute('data-live-link');
+          const projectName = image.getAttribute('data-project-name');
+
+          // Update the modal content
+          modalTitle.textContent = projectName;
+          modalImage.src = image.src;
+          modalDescription.textContent = description;
+          modalBuildWith.innerHTML = ''; // Clear existing list items before loading the ones on currenlt cicked item
+
+          buildWith.split(',').forEach((item) => {
+            const li = document.createElement('li');
+            li.textContent = item.trim();
+            modalBuildWith.appendChild(li);
+          });
+
+          modalSourceButton.href = githubLink;
+          modalLiveButton.href = liveLink;
+
+          // Open the modal
+          const modalInstance = new bootstrap.Modal(modal);
+          modalInstance.show();
+        });
+      });
     }
   });
 };
